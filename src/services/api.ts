2185c72cfa9fdaa9e4c70ea54fbd66ac900b4e0e
Raniere <raniereslourenco@gmail.com>
2021-07-24 12:58:30 -0300

    teste do json

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8888/produtos'
+    // baseURL: 'http://localhost:8888/produtos'
+    baseURL: 'https://github.com/raniereslourenco/Desafio2/blob/main/server.json'
 })
 
 export default api;
